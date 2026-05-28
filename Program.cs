24853eadfcbb67dc3a21524732b60d5927c703f0
River920 <river.m.phoenix@gmail.com>
2026-05-28 20:16:09 +1000

    Added Program.cs code

--- Program.cs
+++ Program.cs
@@ -1,26 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+app.MapGet("/show-users", BackEndpoints.ShowHTMLforIndex);
+
+app.Run("http://localhost:4000");
+
+
+static class BackEndpoints
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static async Task<IResult> ShowHTMLforIndex()
+    {
+        var html = await File.ReadAllTextAsync("index.html");
+        return Results.Content(html, "text/html");
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
-
-app.Run();
