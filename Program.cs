1afc9cc7bf5cc175eb9b01b2bb60ab5272004a22
River920 <river.m.phoenix@gmail.com>
2026-05-29 18:13:25 +1000

    modified web

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/show-users", BackEndpoints.ShowHTMLforIndex);
 
-app.Run();
+app.Run("http://localhost:4000");
+
+
+static class BackEndpoints
+{
+    public static async Task<IResult> ShowHTMLforIndex()
+    {
+        var html = await File.ReadAllTextAsync("index.html");
+        return Results.Content(html, "text/html");
+    }
+}
