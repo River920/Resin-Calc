3da264880d2205d9ba8a59f27827b5c37f03b00b
River Manoukian <river.m.phoenix@gmail.com>
2026-07-26 14:23:10 +1000

    added directory and store options

--- Program.cs
+++ Program.cs
@@ -2,7 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/show-users", BackEndpoints.ShowHTMLforIndex);
+app.MapGet("/show-calculator", BackEndpoints.ShowHTMLforIndex);
+app.MapGet("/show-tutorial", BackEndpoints.ShowHTMLforTutorial);
+app.MapGet("/", BackEndpoints.ShowHTMLforDirectory);
 
 app.Run("http://localhost:4000");
 
@@ -12,6 +14,18 @@ static class BackEndpoints
     public static async Task<IResult> ShowHTMLforIndex()
     {
         var html = await File.ReadAllTextAsync("index.html");
+        return Results.Content(html, "text/html");
+    }
+        
+    public static async Task<IResult> ShowHTMLforTutorial()
+    {
+        var html = await File.ReadAllTextAsync("tutorial.html");
+        return Results.Content(html, "text/html");
+    }
+
+    public static async Task<IResult> ShowHTMLforDirectory()
+    {
+        var html = await File.ReadAllTextAsync("directory.html");
         return Results.Content(html, "text/html");
     }
 }
